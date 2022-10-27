7fc3a24f17e3fdab705bf015a037935cc2762dbb
BentongX <fuenteskevin2015@gmail.com>
2022-10-27 17:57:56 +0800

    Implement ability to reset graph

--- components/frontend/Header.js
+++ components/frontend/Header.js
@@ -1,6 +1,15 @@
-import { Flex, Heading, Text } from "@chakra-ui/react";
+import { useDispatch } from "react-redux";
+import { Flex, Heading, Text, Button } from "@chakra-ui/react";
+import { reset } from "../../redux/slices/barGraph.slice";
 
-function Header() {
+const Header = () => {
+    const dispatch = useDispatch();
+
+    const clear = (e) => {
+        e.preventDefault();
+        dispatch(reset());
+    }
+
     return (
         <Flex py="4rem" flexDir="column" align="center">
             <Heading fontSize="3xl" fontWeight={600}>
@@ -9,8 +18,11 @@ function Header() {
             <Text fontSize="20px" fontWeight={600} color="subtle-text">
                 Kevin Fuentes
             </Text>
+            <Button colorScheme='teal' mt={"50px"} onClick={clear}>
+                RESET GRAPH
+            </Button>
         </Flex>
     );
-}
+};
 
 export default Header;
